977224768af18516b18efcae7e603eaecba4256b
Gopinathgopi13 <gopinathkathirvel13@gmail.com>
2023-12-09 14:28:30 +0530

    Task

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import EmployeeDetails from './components/EmployeeDetails';
+import Home from './components/Home';
+import Login from './components/Login';
+import EmpData from './components/ReusabilityComponents/EmpData';
+import { Routes, Route, Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<Login/>}/>
+
+      
+
+      <Route path='HomePage' element={<Home/>}>
+         <Route path='EmployeeData' element={<EmpData/>}/>
+      </Route>
+      <Route path='EmployeeData' element={<EmpData/>}/>
+    </Routes>
   );
 }
 
---
+++ src/components/EmployeeDetails.jsx
@@ -0,0 +1,58 @@
+import React,{useState} from 'react';
+import i from './styles/Employee.module.css';
+import Footer from './ReusabilityComponents/Footer';
+import Header from './ReusabilityComponents/Header';
+import CompaneyLogo from './ReusabilityComponents/CompaneyLogo';
+import { IoSearch } from "react-icons/io5";
+import SearchBar from './ReusabilityComponents/SearchBar';
+import Table from 'react-bootstrap/Table';
+import EmpData from './ReusabilityComponents/EmpData';
+import Home from './Home';
+
+function EmployeeDetails() {
+  let [home,setHome] =useState(false);
+  let [emp,setEmp] =useState(true);
+  let [temp,setTemp] = useState();
+
+  
+  let hey =(isActive)=>{
+    console.log('home called')
+    if(isActive == 'home active'){
+      setHome(true);
+      setEmp(false);
+      setTemp(isActive);
+    }else if(isActive == 'emp active'){
+      setHome(false);
+      setEmp(true);
+      setTemp(isActive);
+    }
+  }
+  return (
+   <>
+   { emp?
+    <div className={i.grantParent}>
+    <Header className={i.header}/>
+     <div className={i.parentTag}>
+         
+        <CompaneyLogo/>
+        <SearchBar/>
+        
+          <div className={i.empDataParent}>
+              {/* <EmpData/> */}
+         </div>
+       
+     </div>
+     <div className={i.footerHead}>
+ 
+     <Footer Fun ={hey} active={2}/>
+     </div>
+    </div> :null
+   }
+   {
+     home? <Home/> : null
+    }
+   </>
+  )
+}
+
+export default EmployeeDetails
